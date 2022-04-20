3c1ded15ec2bb1e10199aa8057c01b750a05ce4e
Ruchira <ruchirapednekar16@gmail.com>
2022-04-20 16:13:07 +0530

    first commit

---
+++ app/src/main/java/com/example/userregistration/SearchAdapter.java
@@ -0,0 +1,80 @@
+package com.example.userregistration;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SearchAdapter extends FirebaseRecyclerAdapter<SearchModel, SearchAdapter.myViewHolder> {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public SearchAdapter(@NonNull FirebaseRecyclerOptions<SearchModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull SearchModel model) {
+
+        holder.name.setText(model.getName());
+        holder.designation.setText(model.getDesignation());
+        holder.email.setText(model.getEmail());
+        holder.location.setText(model.getLocation());
+
+        Glide.with(holder.img.getContext())
+                .load(model.getImage())
+                .placeholder(R.drawable.common_google_signin_btn_icon_dark)
+                .circleCrop()
+                .error(R.drawable.common_google_signin_btn_icon_dark_normal)
+                .into(holder.img);
+
+
+
+
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_item,parent, false);
+
+        return new myViewHolder(view);
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView img;
+        TextView name, email, designation, location;
+
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            img = (CircleImageView)itemView.findViewById(R.id.img1);
+            name = (TextView)itemView.findViewById(R.id.profile_name);
+            email = (TextView)itemView.findViewById(R.id.email);
+            designation = (TextView)itemView.findViewById(R.id.desig);
+            location = (TextView)itemView.findViewById(R.id.location);
+
+
+
+        }
+    }
+}
